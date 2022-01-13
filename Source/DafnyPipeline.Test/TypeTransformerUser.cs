65403055bf4e7410833b9f5a79740d9700033d3f
Remy Willems <>
2022-01-14 00:39:41 +0100

    update test

--- Source/DafnyPipeline.Test/TypeTransformerUser.cs
+++ Source/DafnyPipeline.Test/TypeTransformerUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Dafny;
 using TypeTransformers;
 using Xunit;
@@ -10,8 +11,14 @@ namespace DafnyPipeline.Test;
 public class TypeTransformerUser {
   [Fact]
   public void Use() {
+    var usings = new [] { 
+      SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System.Collections.Generic")), 
+      SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("Microsoft.Boogie"))
+    };
+    
     TypeTransformer.TransformType(new Uri("/Users/rwillems/Documents/SourceCode/dafny/Source/Dafny/AST/DafnyAst2.cs"),
-      "Microsoft.Dafny.V2", "Microsoft.Dafny", typeof(Program), TypeTransformer.GetUnionsInNamespace(typeof(Program)), 
+      "Microsoft.Dafny.V2", "Microsoft.Dafny", usings,false, true, 
+      typeof(Program), TypeTransformer.GetUnionsInNamespace(typeof(Program)), 
       new Dictionary<Type, TypeMutation>() {
         { typeof(UpdateStmt), new TypeMutation(new HashSet<string>() { nameof(UpdateStmt.Lhss)}, new Dictionary<string, Type>() {
           { "Lhss2", typeof(IReadOnlyList<AssignmentRhs>) }
